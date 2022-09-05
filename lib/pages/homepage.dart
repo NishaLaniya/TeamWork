ba6e9b031192eec813c9fb2ae488bc3686c40dc8
kinjal gajjar <gajjarkinjalbhailalbhai@gmail.com>
2022-09-05 17:40:21 +0530

    Appbar

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,23 +12,48 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: customAppbar(),
       body: Column(
         children: [
-          customAppbar(),
+
         ],
       ),
     );
   }
 }
+notice(){
+  
+}
 
 customAppbar() {
-  return Column(
-    children: const [
-      Text(
-        '11 AUG',
-        style: TextStyle(fontSize: 12),
+  return AppBar(
+    elevation: 0,
+    titleSpacing: 0,
+    actions: [
+      Padding(
+        padding: const EdgeInsets.only(right: 43.75),
+        child: SvgPicture.asset('assets/images/profile.svg'),
       ),
-      Text('Hi, Smit Patel')
     ],
+    backgroundColor: Colors.white,
+    title: Padding(
+      padding: const EdgeInsets.only(left: 43),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Text(
+            '11 AUG',
+            style: TextStyle(
+                fontSize: 12, color: Color(0xff838383), fontFamily: 'Regular'),
+          ),
+          Text(
+            'Hi, Smit Patel',
+            style: TextStyle(
+                fontSize: 16, color: Colors.black, fontFamily: 'Bold'),
+          )
+        ],
+      ),
+    ),
   );
 }
